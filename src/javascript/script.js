01e26841c81deb12c6e4f2c0e7b976c5237ca676
juliasta702@gmail.com <juliasta702@gmail.com>
2023-10-20 19:57:47 +0800

    add dark-mode

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,7 +1,36 @@
 const hamburgerToggle = document.getElementById("hamburger-toggle");
 const navMenu = document.getElementById("nav-menu");
+const html = document.querySelector("html");
 
 hamburgerToggle.addEventListener("click", () => {
   hamburgerToggle.classList.toggle("hamburger-active");
   navMenu.classList.toggle("nav-active");
+});
+
+const toggleSM = document.getElementById("toggle-sm");
+const ligthDarkModeSm = document.getElementById("light-dark-mode-sm");
+toggleSM.addEventListener("click", () => {
+  if (toggleSM.checked) {
+    html.classList.add("dark");
+    ligthDarkModeSm.innerHTML = `<img src="./src/img/moon.png" alt=""
+    />`;
+  } else {
+    html.classList.remove("dark");
+    ligthDarkModeSm.innerHTML = `<img src="./src/img/sun.png" alt=""
+    />`;
+  }
+});
+
+const toggleLG = document.getElementById("toggle-lg");
+const ligthDarkModeLg = document.getElementById("light-dark-mode-lg");
+toggleLG.addEventListener("click", () => {
+  if (toggleLG.checked) {
+    html.classList.add("dark");
+    ligthDarkModeLg.innerHTML = `<img src="./src/img/moon.png" alt=""
+    />`;
+  } else {
+    html.classList.remove("dark");
+    ligthDarkModeLg.innerHTML = `<img src="./src/img/sun.png" alt=""
+    />`;
+  }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 6s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
